ef797d4287351af02f36a69c12c2da980e486c8c
Vasilev-Stefan <sbasilev@gmail.com>
2025-11-14 13:44:13 +0200

    Add Search component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import { Header } from "./components/Header.jsx";
+import { Search } from "./components/Search.jsx";
 
 function App() {
 
   return (
     <>
       <Header />
+      <div>
+        <Search />
+      </div>
     </>
   )
 }
