cb8d13827fc65e24af6bf095fb435f10db8b7f72
Vasilev-Stefan <sbasilev@gmail.com>
2025-11-14 18:50:32 +0200

    Add the ability to create new users

--- client/src/components/CreateUser.jsx
+++ client/src/components/CreateUser.jsx
@@ -1,4 +1,29 @@
 export function CreateUser({createUserHandle}) {
+
+    function CreateUserSubmit(e) {
+        e.preventDefault()
+
+        const formData = new FormData(e.target)
+        const {city, country, street, streetNumber, ...data} = Object.fromEntries(formData)
+        data.address = {country, city, street,streetNumber}
+        data.createdAt = new Date().toISOString()
+        data.updatedAt = new Date().toISOString()
+
+        if(!data.firstName && !data.lastName && !data.email && !data.phoneNumber && !data.imageUrl && !data.address && !data.createdAt && !data.updatedAt){
+            alert('All fields are requiered!');
+            return;
+        }
+
+        console.log(data)
+        fetch('http://localhost:3030/jsonstore/users', {
+            method: 'post',
+            headers: {
+                "Content-Type": 'application/json'       
+            },
+            body: JSON.stringify(data)
+        })
+    }
+
     return (
         <div className="overlay">
             <div className="backdrop" onClick={createUserHandle}></div>
@@ -15,17 +40,17 @@ export function CreateUser({createUserHandle}) {
                             </svg>
                         </button>
                     </header>
-                    <form>
+                    <form onSubmit={CreateUserSubmit}>
                         <div className="form-row">
                             <div className="form-group">
-                                <label for="firstName">First name</label>
+                                <label htmlFor="firstName">First name</label>
                                 <div className="input-wrapper">
                                     <span><i className="fa-solid fa-user"></i></span>
                                     <input id="firstName" name="firstName" type="text" />
                                 </div>
                             </div>
                             <div className="form-group">
-                                <label for="lastName">Last name</label>
+                                <label htmlFor="lastName">Last name</label>
                                 <div className="input-wrapper">
                                     <span><i className="fa-solid fa-user"></i></span>
                                     <input id="lastName" name="lastName" type="text" />
@@ -35,14 +60,14 @@ export function CreateUser({createUserHandle}) {
 
                         <div className="form-row">
                             <div className="form-group">
-                                <label for="email">Email</label>
+                                <label htmlFor="email">Email</label>
                                 <div className="input-wrapper">
                                     <span><i className="fa-solid fa-envelope"></i></span>
                                     <input id="email" name="email" type="text" />
                                 </div>
                             </div>
                             <div className="form-group">
-                                <label for="phoneNumber">Phone number</label>
+                                <label htmlFor="phoneNumber">Phone number</label>
                                 <div className="input-wrapper">
                                     <span><i className="fa-solid fa-phone"></i></span>
                                     <input id="phoneNumber" name="phoneNumber" type="text" />
@@ -51,7 +76,7 @@ export function CreateUser({createUserHandle}) {
                         </div>
 
                         <div className="form-group long-line">
-                            <label for="imageUrl">Image Url</label>
+                            <label htmlFor="imageUrl">Image Url</label>
                             <div className="input-wrapper">
                                 <span><i className="fa-solid fa-image"></i></span>
                                 <input id="imageUrl" name="imageUrl" type="text" />
@@ -60,14 +85,14 @@ export function CreateUser({createUserHandle}) {
 
                         <div className="form-row">
                             <div className="form-group">
-                                <label for="country">Country</label>
+                                <label htmlFor="country">Country</label>
                                 <div className="input-wrapper">
                                     <span><i className="fa-solid fa-map"></i></span>
                                     <input id="country" name="country" type="text" />
                                 </div>
                             </div>
                             <div className="form-group">
-                                <label for="city">City</label>
+                                <label htmlFor="city">City</label>
                                 <div className="input-wrapper">
                                     <span><i className="fa-solid fa-city"></i></span>
                                     <input id="city" name="city" type="text" />
@@ -77,14 +102,14 @@ export function CreateUser({createUserHandle}) {
 
                         <div className="form-row">
                             <div className="form-group">
-                                <label for="street">Street</label>
+                                <label htmlFor="street">Street</label>
                                 <div className="input-wrapper">
                                     <span><i className="fa-solid fa-map"></i></span>
                                     <input id="street" name="street" type="text" />
                                 </div>
                             </div>
                             <div className="form-group">
-                                <label for="streetNumber">Street number</label>
+                                <label htmlFor="streetNumber">Street number</label>
                                 <div className="input-wrapper">
                                     <span><i className="fa-solid fa-house-chimney"></i></span>
                                     <input id="streetNumber" name="streetNumber" type="text" />
